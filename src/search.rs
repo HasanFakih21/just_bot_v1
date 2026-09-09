bc2684577086f38705212ccf7e622af6e96702ba
hasanfakih21 <hasanfakih@cmail.carleton.ca>
2026-09-09 03:32:49 -0400

    bench: 16342183

--- src/search.rs
+++ src/search.rs
@@ -294,12 +294,17 @@ pub fn search<Node: NodeType>(
     if !Node::ROOT
         && !in_check
         && !excluded
+        && data.stack[ply - 1].m.is_null()
         && data.stack[ply - 1].m.kind().is_quiet()
         && data.stack[ply - 1].eval != Score::NONE
     {
         let bonus = (512 * (-data.stack[ply - 1].eval - static_eval) / 128).clamp(-1200, 1200);
-        data.quiet_history
-            .update(data.stack[ply - 1].threats, !stm, data.stack[ply - 1].m, bonus);
+        data.quiet_history.update(
+            data.stack[ply - 1].threats,
+            !stm,
+            data.stack[ply - 1].m,
+            (bonus.abs() - depth * 200).max(0) * bonus.signum(),
+        );
     }
 
     if !Node::ROOT && !in_check && !excluded && data.stack[ply - 1].eval != Score::NONE {
